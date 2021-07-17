6c2843b1a1aa28d14eb56d2312574ce52abb050b
salim <zubairov.salim@yandex.ru>
2021-07-17 16:10:44 +0500

    added screen for adding  new places, added redux and redux-thunk setup

--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView, View, Text, TextInput, Button, StyleSheet} from 'react-native';
+import {useDispatch} from 'react-redux';
+import Colors from '../constants/Colors';
+import {addPlace} from '../store/places-actions';
 
 const NewPlacesScreen = props => {
+    const {titleValue, setTitleValue} = useState('');
+
+    const dispatch = useDispatch();
+
+    const titleChangeHandler = text => {
+        setTitleValue(text);
+    };
+
+    const savePlaceHandler = () => {
+        dispatch(addPlace(titleValue));
+        props.navigation.goBack();
+    };
+
     return (
-        <View>
-            <Text>NewPlacesScreen</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                    style={styles.textInput}
+                    value={titleValue}
+                    onChangeText={titleChangeHandler}
+                />
+                <Button
+                    title="Save Place"
+                    color={Colors.primary}
+                    onPress={() => {
+                        savePlaceHandler();
+                    }}
+                />
+            </View>
+        </ScrollView>
     )
 };
 
@@ -14,7 +44,20 @@ NewPlacesScreen.navigationOptions  = {
 };
 
 const styles = StyleSheet.create({
-
+    form: {
+        margin: 30
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2
+    }
 });
 
 export default NewPlacesScreen;
